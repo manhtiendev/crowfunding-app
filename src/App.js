a70bb9ee13acedac8538023d644d8d0e6a479082
manhtiendev <manhtiendev1307@gmail.com>
2024-06-24 17:37:52 +0700

    Payment, Checkout, Shipping pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import CampaignView from 'modules/campaign/CampaignView';
 import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Modal from 'react-modal';
+import LayoutPayment from 'layout/LayoutPayment';
 
 const SignUpPage = lazy(() => import('./pages/SignUpPage'));
 const SignInPage = lazy(() => import('./pages/SignInPage'));
 const DashboardPage = lazy(() => import('./pages/DashboardPage'));
 const CampaignPage = lazy(() => import('./pages/CampaignPage'));
 const StartCampaignPage = lazy(() => import('./pages/StartCampaignPage'));
+const CheckoutPage = lazy(() => import('./pages/CheckoutPage'));
+const ShippingPage = lazy(() => import('./pages/ShippingPage'));
+const PaymentPage = lazy(() => import('./pages/PaymentPage'));
 
 // const customStyles = {
 //   content: {},
@@ -27,6 +31,11 @@ function App() {
           <Route path='/campaign' element={<CampaignPage></CampaignPage>}></Route>
           <Route path='/start-campaign' element={<StartCampaignPage></StartCampaignPage>}></Route>
           <Route path='/campaign/:slug' element={<CampaignView></CampaignView>}></Route>
+        </Route>
+        <Route element={<LayoutPayment></LayoutPayment>}>
+          <Route path='/checkout' element={<CheckoutPage></CheckoutPage>}></Route>
+          <Route path='/shipping-address' element={<ShippingPage></ShippingPage>}></Route>
+          <Route path='/payment' element={<PaymentPage></PaymentPage>}></Route>
         </Route>
         <Route path='/sign-up' element={<SignUpPage></SignUpPage>}></Route>
         <Route path='/sign-in' element={<SignInPage></SignInPage>}></Route>
